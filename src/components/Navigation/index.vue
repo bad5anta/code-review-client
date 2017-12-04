ad635ae963249a198a955072dcc922c537c63c53
faul <faullik@gmail.com>
2017-12-04 12:05:54 +0200

    Routes

---
+++ src/components/Navigation/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="navigation">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Navigation',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.navigation {
+
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import DiffPage from '@/components/DiffPage';
+import Dashboard from '@/components/Dashboard';
+import UserDashboard from '@/components/UserDashboard';
 
 Vue.use(Router);
 
@@ -8,8 +10,20 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      name: 'dashboard',
+      component: Dashboard,
+      children: [
+        {
+          path: 'user/:userId',
+          component: UserDashboard,
+          children: [
+            {
+              path: 'diff/diffId',
+              component: DiffPage,
+            },
+          ],
+        },
+      ],
     },
   ],
 });

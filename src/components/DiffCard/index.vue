93d6a48f0a1b27f8ff97d6a6d41d097f5908afae
faul <faullik@gmail.com>
2017-12-04 13:45:06 +0200

    Add badge

--- src/components/DiffCard/index.vue
+++ src/components/DiffCard/index.vue
@@ -6,6 +6,7 @@
         </span>
       </h4>
       <h6 class="card-subtitle mb-2 text-muted">Created {{date}}</h6>
+      <p><b-badge>{{item.technology}}</b-badge></p>
       <b-button :to="cardLink" variant="primary" v-if="item.status === 'done'">View diff</b-button>
       <b-button :to="cardLink" variant="primary" v-if="item.status === 'new'">Add review</b-button>
     </b-card>
